7fa4fb30d8a32251b15aca8f2f1d1b176c75525e
huan-shi <huan.shi@microfocus.com>
2020-07-10 17:00:03 +0800

    fix issue

--- kata/bowling/bowling.py
+++ kata/bowling/bowling.py
@@ -1,43 +1,38 @@
-class Roll(object):
-	def __init__(self):
-		self.score = 0
-
-	def set_score(self, score):
-		self.score = score
-
-
 class Frame(object):
 	def __init__(self):
 		self.score = 0
-		self.num_scoring_roll = 2
-		self.rolls = list()
+		self.max_scoring_roll = 2
+		self.roll_scores = list()
 		self.active = True
 		self.scoring_enable = True
 
-	def add_roll(self, roll):
-		if len(self.rolls) < self.num_scoring_roll:
-			self.rolls.append(roll)
-			self.score = sum(r.score for r in self.rolls)
+	def add_score(self, roll_score):
+		if len(self.roll_scores) < self.max_scoring_roll:
+			self.roll_scores.append(roll_score)
+			self.score = sum(self.roll_scores)
 			self.handle_for_bonus()
-		else:
+		self.check_frame_active()
+
+	def check_frame_active(self):
+		if len(self.roll_scores) >= self.max_scoring_roll:
 			self.scoring_enable = False
 			self.active = False
 
 	def handle_for_bonus(self):
 		if self.is_strike() or self.is_spare():
 			self.active = False
-			self.num_scoring_roll = 3
-		elif len(self.rolls) >= 2:
+			self.max_scoring_roll = 3
+		elif len(self.roll_scores) >= 2:
 			self.active = False
 
 	def is_strike(self):
-		if len(self.rolls) == 1 and self.rolls[0].score == 10:
+		if len(self.roll_scores) == 1 and self.roll_scores[0] == 10:
 			return True
 		else:
 			return False
 
 	def is_spare(self):
-		if len(self.rolls) == 2 and sum([self.rolls[0].score, self.rolls[1].score]) == 10:
+		if len(self.roll_scores) == 2 and sum(self.roll_scores) == 10:
 			return True
 		else:
 			return False
@@ -58,27 +53,56 @@ class Game(object):
 		self.num_frame = 1
 
 	def play(self, score):
-		if not self.is_game_over:
-			roll = Roll()
-			roll.set_score(score)
-			for frame in self.scoring_frames:
-				frame.add_roll(roll)
-			if not self.scoring_frames[-1].active:
+		if not self.is_game_over():
+			for f in self.scoring_frames:
+				f.add_score(score)
+			if self.num_frame < self.max_frame and not self.scoring_frames[-1].active:
 				frame = Frame()
 				self.scoring_frames.append(frame)
 				self.num_frame += 1
+			self.calc_score()
 
 	def calc_score(self):
-		scoring_disabled_frames = list()
+		scoring_completed_frames = list()
 		for frame in self.scoring_frames:
 			if not frame.scoring_enable:
-				scoring_disabled_frames.append(frame)
-		for frame in scoring_disabled_frames:
+				scoring_completed_frames.append(frame)
+		for frame in scoring_completed_frames:
 			self.scoring_frames.remove(frame)
 			self.score += frame.score
 
 	def is_game_over(self):
-		if self.num_frame >= self.max_frame and len(self.scoring_frames) == 0:
+		if self.num_frame > self.max_frame and len(self.scoring_frames) == 0:
 			return True
 		else:
 			return False
+
+
+if __name__ == "__main__":
+	def test_strike():
+		game = Game()
+		for i in range(10):
+			game.play(10)
+		game.play(10)
+		game.play(10)
+		print(game.score)
+
+	def test_spare():
+		game = Game()
+		for i in range(10):
+			game.play(5)
+			game.play(5)
+		game.play(5)
+		print(game.score)
+
+
+	def test_miss():
+		game = Game()
+		for i in range(10):
+			game.play(9)
+			game.play(0)
+		print(game.score)
+
+	test_strike()
+	test_spare()
+	test_miss()
--- kata/bowling/test_bowling.py
+++ kata/bowling/test_bowling.py
@@ -3,28 +3,28 @@ from bowling import Game
 
 
 class TestBowlingGame(unittest.TestCase):
-	def all_spare(self):
+	def test_all_spare(self):
 		game = Game()
 		for i in range(10):
 			game.play(5)
 			game.play(5)
 		game.play(5)
-		self.assertEqual(game.score, 150)
+		self.assertEqual(150, game.score)
 
-	def all_strike(self):
+	def test_all_strike(self):
 		game = Game()
 		for i in range(10):
 			game.play(10)
 		game.play(10)
 		game.play(10)
-		self.assertEqual(game.score, 300)
+		self.assertEqual(300, game.score)
 
-	def miss_frame(self):
+	def test_miss_frame(self):
 		game = Game()
 		for i in range(10):
 			game.play(9)
 			game.play(0)
-		self.assertEqual(game.score, 90)
+		self.assertEqual(90, game.score)
 
 
 if __name__ == '__main__':
